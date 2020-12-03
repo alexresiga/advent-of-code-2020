52408a7221db6d0abf072ea326757b56bb3cccac
Alex Resiga <resiga.alex@gmail.com>
2020-12-03 07:55:32 +0200

    (now awake) refactoring of day three

--- day03.py
+++ day03.py
@@ -2,19 +2,16 @@ from itertools import starmap
 from math import prod
 
 grid = [list(x.strip()) for x in open('data.in').readlines()]
-for line in grid:
-    line.extend(line * len(grid))
 
 
 def trajectory(a, b):
     result = 0
     i = j = 0
-
-    while i < len(grid) and j < len(grid[i]):
-        if i < len(grid) and j < len(grid[i]) and grid[i][j] == '#':
+    while i < len(grid):
+        if grid[i][j] == '#':
             result += 1
+        j = (j + b) % len(grid[i])
         i += a
-        j += b
     return result
 
 
